1d44b172d51ab339ea1ae615cd08ac4f3494fc65
Ubuntu <ubuntu@ip-172-31-19-217.eu-central-1.compute.internal>
2015-12-04 16:39:52 +0000

    It just reverses the message received, if it is text

--- anicalbot.py
+++ anicalbot.py
@@ -15,7 +15,7 @@ class MessageCounter(telepot.helper.ChatHandler):
     @asyncio.coroutine
     def on_message(self, msg):
         if "text" in msg:
-            yield from self.sender.sendMessage(msg[::-1])
+            yield from self.sender.sendMessage(msg["text"][::-1])
 
 TOKEN = sys.argv[1]  # get token from command-line
 
